97039cd8d749ec2966c918a7d2064fcb9f731075
exodus <quentin.lelan@etudiant.univ-rennes1.fr>
2024-02-12 17:59:28 +0100

    add test

--- SR_Scala_Project/src/test/scala/ServerTest.scala
+++ SR_Scala_Project/src/test/scala/ServerTest.scala
@@ -26,13 +26,15 @@ class ServerTest extends AnyFunSuite with Matchers with ScalatestRouteTest {
 
   test("should respond with correct message") {
     assertWebsocket("Bob"){ wsClient =>
-      wsClient.expectMessage("[{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0}}]")
+      val receivedMessage = wsClient.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0},\"score\":0}], \"foods\": ["))
     }
   }
 
   test("should register player") {
     assertWebsocket("Bob"){ wsClient =>
-      wsClient.expectMessage("[{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0}}]")
+        val receivedMessage = wsClient.expectMessage()
+        assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0},\"score\":0}], \"foods\": ["))
     }
   }
 
@@ -42,25 +44,89 @@ class ServerTest extends AnyFunSuite with Matchers with ScalatestRouteTest {
     val wsClientAlice = WSProbe()
 
     WS(s"/?playerName=Bob", wsClientBob.flow) ~> gameService.websocketRoute ~> check {
-      wsClientBob.expectMessage("[{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0}}]")
+      val receivedMessage = wsClientBob.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0},\"score\":0}], \"foods\": ["))
     }
 
     WS(s"/?playerName=Alice", wsClientAlice.flow) ~> gameService.websocketRoute ~> check {
-      wsClientAlice.expectMessage("[{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0}},{\"name\":\"Alice\",\"position\":{\"x\":0,\"y\":0}}]")
+      val receivedMessage = wsClientAlice.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0},\"score\":0},{\"name\":\"Alice\",\"position\":{\"x\":0,\"y\":0},\"score\":0}], \"foods\": ["))
     }
   }
 
   test("should register player and move it up") {
     assertWebsocket("Bob"){ wsClient =>
-      wsClient.expectMessage("[{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0}}]")
+      var receivedMessage = wsClient.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":0}"))
       wsClient.sendMessage("up")
-      wsClient.expectMessage("[{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":1}}]")
+      receivedMessage = wsClient.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":1}"))
     }
   }
 
+  test("should have collision"){
+    val gameService = new GameService()
+    val wsClientBob = WSProbe()
+    val wsClientAlice = WSProbe()
+
+    WS(s"/?playerName=Bob", wsClientBob.flow) ~> gameService.websocketRoute ~> check {
+      var receivedMessage = wsClientBob.expectMessage()
+      wsClientBob.sendMessage("up")
+      receivedMessage = wsClientBob.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":1}"))
+    }
+
+    WS(s"/?playerName=Alice", wsClientAlice.flow) ~> gameService.websocketRoute ~> check {
+      var receivedMessage = wsClientAlice.expectMessage()
+      wsClientAlice.sendMessage("up")
+      receivedMessage = wsClientAlice.expectMessage()
+      assert(receivedMessage.toString().contains("{\"players\": [{\"name\":\"Bob\",\"position\":{\"x\":0,\"y\":1},\"score\":0},{\"name\":\"Alice\",\"position\":{\"x\":0,\"y\":0}"))
+    }
+  }
+
+  test("should have food"){
+    assertWebsocket("Bob"){ wsClient =>
+      val receivedMessage = wsClient.expectMessage()
+      assert(!receivedMessage.toString().contains("\"score\":10}], \"foods\": []"))
+    }
+  }
+
+  test("should eat all food"){
+    val gameService = new GameService()
+    val wsClientBob = WSProbe()
+    val wsClientAlice = WSProbe()
+
+    WS(s"/?playerName=Bob", wsClientBob.flow) ~> gameService.websocketRoute ~> check {
+      wsClientBob.sendMessage("up")
+      wsClientBob.sendMessage("down")
+      var receivedMessage = wsClientBob.expectMessage()
+      for (_ <- 1 to 3) {
+        for (_ <- 1 to 6) {
+          wsClientBob.sendMessage("up")
+          wsClientBob.expectMessage()
+        }
+        wsClientBob.sendMessage("right")
+        for (_ <- 1 to 6) {
+          wsClientBob.sendMessage("down")
+          wsClientBob.expectMessage()
+        }
+        wsClientBob.sendMessage("right")
+      }
+      for (_ <- 1 to 6) {
+        wsClientBob.sendMessage("down")
+        wsClientBob.expectMessage()
+      }
+
+      receivedMessage = wsClientBob.expectMessage()
+      assert(receivedMessage.toString().contains("\"score\":10}], \"foods\": []}"))
+    }
+  }
+   //test collision avec bordure
   def assertWebsocket(playerName: String)( assertions:(WSProbe) => Unit) : Unit = {
     val gameService = new GameService()
     val wsClient = WSProbe()
     WS(s"/?playerName=$playerName", wsClient.flow) ~> gameService.websocketRoute ~> check(assertions(wsClient))
   }
+
+  //
 }
